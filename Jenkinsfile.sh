a0654f6ec5cb4111e5cd732f540076b34235b3cb
vijaysharma212 <61971396+vijaysharma212@users.noreply.github.com>
2020-03-13 15:58:04 +0530

    Add files via upload

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,11 +1,33 @@
-Jenkinsfile (Declarative Pipeline)
+// Declarative //
 pipeline {
-    agent { docker { image 'maven:3.3.3' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'mvn --version'
-            }
-        }
-    }
+agent any
+stages {
+stage('Build') {
+steps {
+echo 'Building..'
+}
+}
+stage('Test') {
+steps {
+echo 'Testing..'
+}
+}
+stage('Deploy') {
+steps {
+echo 'Deploying....'
+}
+}
+}
+}
+// Script //
+node {
+stage('Build') {
+echo 'Building....'
+}
+stage('Test') {
+echo 'Building....'
+}
+stage('Deploy') {
+echo 'Deploying....'
+}
 }
